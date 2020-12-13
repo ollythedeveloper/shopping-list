c159aa2fcfcbd9c64b382358db0e43e0e363457c
Olivia Lewis <ollybaby12@gmail.com>
2020-12-13 01:12:17 -0500

    added delete funtion to index.js

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ $(function(){
             event.preventDefault();
             //create a constant for the user input
             const userItem = $('#shopping-list-entry').val();
+            //after item submitted empty the 
             $('#shopping-list-entry').val('');
             
             $('.shopping-list').append(`<li>
                 <span class="shopping-item">${userItem}</span>
                 <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-                </div></li>`);
-
+                <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+                </button>
+                </div>
+                </li>`);
             });
-        });
+
+    $('.shopping-list').on ('click', '.shopping-item-delete', function(event){
+        $(this).closest('li').remove();
+    })
+});
